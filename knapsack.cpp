a92f724982260ecf5ee9a35500c6c5cc017a1701
Dereck Santiago <dereckprstgo@yahoo.com>
2026-04-08 11:45:56 -0400

    finished 0/1 knapsack file using memoization

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,7 +1,36 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 // TODO: 0/1 knapsack using DP
-int knapsack(vector<int>& weights, vector<int>& values, int capacity) {
-    return 0;
+int knapsackRecurr(vector<int>& weights, vector<int>& values, int capacity, int currItem, vector<vector<int>>& memo){
+    int result = 0;
+
+    if (currItem == weights.size() || capacity == 0){ //checks if currItem checked each item or capacity is 0
+        return 0;
+    }
+
+    if (memo[currItem][capacity] != -1){ //checks if memo has been occupied by the current item and capacity combo
+        return memo[currItem][capacity];
+    }
+
+    else if (weights[currItem] > capacity){  //if current item doesnt fit in capacity, go to next item
+        memo[currItem][capacity] = knapsackRecurr(weights, values, capacity, currItem + 1, memo);
+    }
+    else{ //else check if we take the next item or not recursively
+        memo[currItem][capacity]  = max(values[currItem] + knapsackRecurr(weights, values, capacity - weights[currItem], currItem + 1, memo),
+                                    knapsackRecurr(weights, values, capacity, currItem + 1, memo)); 
+    }
+    
+    result = memo[currItem][capacity];
+
+    return result;
+}
+
+int knapsack(vector<int>& weights, vector<int>& values, int capacity){
+    
+    int currItem = 0;
+    vector<vector<int>> memo(weights.size(), vector<int>(capacity + 1, -1));//initializes memo and sets every slot to -1 so we know it is empty
+
+    return knapsackRecurr(weights, values, capacity, currItem, memo);
 }
